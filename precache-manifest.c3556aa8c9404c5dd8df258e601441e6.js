f698ed33c0ac94fd997f362d3b1e2c12d75cc604
Jorge Perdomo <joaperdomove@unal.edu.co>
2020-02-12 01:12:29 -0500

    Updates

---
+++ precache-manifest.c3556aa8c9404c5dd8df258e601441e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb89c0c9c298704da67785f0f318b8a7",
+    "url": "/pokefinder/index.html"
+  },
+  {
+    "revision": "faa27eaae2c12733ef79",
+    "url": "/pokefinder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3a3a3796094caed175e1",
+    "url": "/pokefinder/static/css/main.352c68fa.chunk.css"
+  },
+  {
+    "revision": "faa27eaae2c12733ef79",
+    "url": "/pokefinder/static/js/2.01e35926.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokefinder/static/js/2.01e35926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3a3796094caed175e1",
+    "url": "/pokefinder/static/js/main.7a2addd0.chunk.js"
+  },
+  {
+    "revision": "2fdc32246006f0d6f939",
+    "url": "/pokefinder/static/js/runtime-main.bd3533a5.js"
+  }
+]);
